f28e15542f2bf64ce8be54331871da66f614886e
larspauwels <lars.pauwels@telenet.be>
2020-11-24 17:57:45 +0100

    Dark Stories init

---
+++ main.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const dotenv = require('dotenv');
+dotenv.config();
+
+//Controllers
+const Command = require('./controllers/command');
+
+/*
+	FUNCTIONALITIES
+	 * Voice chat Black Stories (with DM)
+	 * 2sec horror stories
+*/
+
+Client.once('ready', () => {
+	console.log('Bot started');
+
+	Command(Client, process.env.GAME, message => {
+		console.log('Typed punish');
+		Punish(message, Client);
+	});
+});
+
+Client.login(process.env.BOT_TOKEN);
+console.log('Bot logged in');
